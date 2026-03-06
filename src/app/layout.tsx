162ab2ec6757045acf01eb7313c974d7be82c0e6
Lucas Sá <tavareslucas383@gmail.com>
2026-03-06 00:27:22 +0000

    feat: implementar estrutura básica raiz

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import type {Metadata} from "next";
+import {inter} from "./ui/fonts";
+import "./globals.css";
+import React from "react";
+
+export const metadata: Metadata ={
+    title:"ACME"
+};
+
+export default function RootLayout({children }: {children: React.ReactNode}){
+ return (
+    <html>
+        <body className= { inter.className }>{children}</body>
+    </html>
+ ); 
+};
